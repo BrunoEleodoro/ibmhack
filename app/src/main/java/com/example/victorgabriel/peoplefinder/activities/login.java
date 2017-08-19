a70fcb3331bb4a66cbffe4f5751bb4903c5b5f34
Victor Gabriel Vasconcelos Bilis <victor_gabriel.bilis@hotmail.com>
2017-08-19 19:08:36 -0300

    Tela de cadastro e login

--- app/src/main/java/com/example/victorgabriel/peoplefinder/activities/login.java
+++ app/src/main/java/com/example/victorgabriel/peoplefinder/activities/login.java
@@ -5,18 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.victorgabriel.peoplefinder.R;
+import com.example.victorgabriel.peoplefinder.tasks.getLogin;
 
 public class login extends AppCompatActivity {
 
     Button btn_cad;
     Button btn_entrar;
 
+    EditText login;
+    EditText senha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (EditText) findViewById(R.id.editText);
+        senha = (EditText) findViewById(R.id.editText2);
 
         btn_cad = (Button) findViewById(R.id.button3);
         btn_entrar = (Button) findViewById(R.id.button2);
@@ -32,6 +40,9 @@ public class login extends AppCompatActivity {
         btn_entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                new getLogin(login.this,login.getText().toString().trim(),senha.getText().toString()).execute("");
+
                 Intent i = new Intent(login.this,Maps_des.class);
                 startActivity(i);
             }
