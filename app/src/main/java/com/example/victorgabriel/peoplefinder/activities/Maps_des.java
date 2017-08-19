91aa00d4546fdede4877f4cfdf3d6d40941b12ca
bruno <brunoeleodoro96@gmail.com>
2017-08-19 20:30:51 -0300

    primeiro teste com a key do maps

--- app/src/main/java/com/example/victorgabriel/peoplefinder/activities/Maps_des.java
+++ app/src/main/java/com/example/victorgabriel/peoplefinder/activities/Maps_des.java
@@ -1,6 +1,8 @@
 package com.example.victorgabriel.peoplefinder.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -9,9 +11,12 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.victorgabriel.peoplefinder.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class Maps_des extends AppCompatActivity {
-
+public class Maps_des extends AppCompatActivity implements OnMapReadyCallback {
+    LocationManager locationManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,9 +25,6 @@ public class Maps_des extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-
-                    return true;
-                case R.id.navigation_dashboard:
 
                     return true;
                 case R.id.navigation_notifications:
@@ -40,10 +42,15 @@ public class Maps_des extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_des);
-
-
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
