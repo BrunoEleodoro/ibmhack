cf841e8a2ee6d2d1eba30630b6b1a8749cabe231
Victor Gabriel Vasconcelos Bilis <victor_gabriel.bilis@hotmail.com>
2017-08-19 16:00:05 -0300

    Alterações Telas e Classes

---
+++ app/src/main/java/com/example/victorgabriel/peoplefinder/activities/cadastro_des.java
@@ -0,0 +1,15 @@
+package com.example.victorgabriel.peoplefinder.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.victorgabriel.peoplefinder.R;
+
+public class cadastro_des extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_des);
+    }
+}
--- app/src/main/java/com/example/victorgabriel/peoplefinder/activities/login.java
+++ app/src/main/java/com/example/victorgabriel/peoplefinder/activities/login.java
@@ -1,15 +1,41 @@
 package com.example.victorgabriel.peoplefinder.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.victorgabriel.peoplefinder.R;
 
 public class login extends AppCompatActivity {
 
+    Button btn_cad;
+    Button btn_entrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_cad = (Button) findViewById(R.id.button3);
+        btn_entrar = (Button) findViewById(R.id.button2);
+
+        btn_cad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(login.this,cadastro.class);
+                startActivity(i);
+            }
+        });
+
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(login.this,Maps_des.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
