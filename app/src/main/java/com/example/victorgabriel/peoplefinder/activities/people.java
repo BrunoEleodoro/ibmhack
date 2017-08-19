97763dc0a443a0e96f5fa03f1dcb3801f5931f28
Victor Gabriel Vasconcelos Bilis <victor_gabriel.bilis@hotmail.com>
2017-08-19 18:44:26 -0300

    Mandando nova configurações

--- app/src/main/java/com/example/victorgabriel/peoplefinder/activities/people.java
+++ app/src/main/java/com/example/victorgabriel/peoplefinder/activities/people.java
@@ -1,19 +1,27 @@
 package com.example.victorgabriel.peoplefinder.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.example.victorgabriel.peoplefinder.Database;
+import com.example.victorgabriel.peoplefinder.Desaparecido;
 import com.example.victorgabriel.peoplefinder.R;
 import com.example.victorgabriel.peoplefinder.tasks.listDesaparecidos;
+import com.example.victorgabriel.peoplefinder.tasks.votarSpam;
 
 public class people extends AppCompatActivity {
 
     ListView lv;
+    Database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,56 @@ public class people extends AppCompatActivity {
 
         lv = (ListView) findViewById(R.id.list);
 
+        db = new Database(this);
+
         new listDesaparecidos(this,lv,"","","","").execute("");
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+
+            }
+        });
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                final Desaparecido des = new Desaparecido();
+
+                AlertDialog.Builder detalhes = new AlertDialog.Builder(people.this);
+                detalhes.setTitle("Spam");
+                detalhes.setMessage("Deseja denunciar esse desaparecimento como spam?");
+                detalhes.setIcon(R.drawable.icon_dois);
+                detalhes.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        String cod = null;
+
+                        Cursor c = db.select("SELECT * FROM login");
+                        while(c.moveToNext())
+                        {
+                            cod = c.getString(c.getColumnIndex("cod"));
+                        }
+
+                        new votarSpam(people.this,""+des.getCod(),cod).execute("");
+
+                    }
+                });
+                detalhes.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                detalhes.show();
+
+                return false;
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/victorgabriel/peoplefinder/ver_des.java
+++ app/src/main/java/com/example/victorgabriel/peoplefinder/ver_des.java
@@ -3,11 +3,11 @@ package com.example.victorgabriel.peoplefinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class cadastro extends AppCompatActivity {
+public class ver_des extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_ver_des);
     }
 }
